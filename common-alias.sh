27762ba5f5b7350fb3c2e6eca59699e562d14e07
Vikas Sharma <42124009+vos139@users.noreply.github.com>
2026-02-20 09:30:41 +0000

    Create common-alias.sh

---
+++ common-alias.sh
@@ -0,0 +1,9 @@
+source /Users/hz480957/.kube-config-get.sh
+alias k="kubectl"
+alias kn="kubectl get ns"
+alias kcg="kubectl config get-contexts"
+alias kcu="kubectl config use-context"
+alias kcc="kubectl config current-context"
+alias ksn="kubectl config set-context --current --namespace"
+alias kn="kubectl get ns"
+alias kp="kubectl get po"
